230ce56fa180d25fcf66e9e5a01860c044244200
Julien CROCHET <julien@crochet.me>
2021-03-02 23:28:28 +0100

    Replace JMS Handlers by Symfony Normalizer for exceptions

---
+++ apps/api/src/EventListener/ExceptionSubscriber.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Normalizer\ExceptionNormalizerFactory;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Exception Subscriber / Listener : Ensure to always return a JSON response
+ * when an exception is thrown.
+ *
+ * @author Julien CROCHET <julien@crochet.me>
+ */
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var ExceptionNormalizerFactory
+     */
+    private $normalizerFactory;
+
+    public function __construct(ExceptionNormalizerFactory $normalizerFactory)
+    {
+        $this->normalizerFactory = $normalizerFactory;
+    }
+
+    /**
+     * Return the subscribed events, their methods and priorities.
+     *
+     * @codeCoverageIgnore
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'processException',
+        ];
+    }
+
+    /**
+     * Replace exception response by a JSON stream.
+     *
+     * @return void
+     */
+    public function processException(ExceptionEvent $event)
+    {
+        $response = new Response();
+        $exception = $event->getThrowable();
+
+        $response = $this->createApiResponse($exception);
+        $event->setResponse($response);
+    }
+
+    /**
+     * Creates the JsonResponse from any Exception.
+     *
+     * @return JsonResponse
+     */
+    private function createApiResponse(\Exception $exception)
+    {
+        $normalizer = $this->normalizerFactory->getNormalizer($exception);
+        $code = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        try {
+            $errors = $normalizer ? $normalizer->normalize($exception) : [];
+        } catch (\Exception $e) {
+            $errors = [];
+        }
+
+        $response = [
+            'code' => $code,
+            'message' => $exception->getMessage(),
+        ];
+
+        if ($errors) {
+            $response['errors'] = $errors;
+        }
+
+        return new JsonResponse($response);
+    }
+}
---
+++ apps/api/src/Normalizer/FormExceptionNormalizer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Normalizer;
+
+use App\Exception\FormException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * FormExceptionNormalizer
+ * Format a FormException for API Response.
+ *
+ * @author Julien CROCHET <julien@crochet.me>
+ */
+class FormExceptionNormalizer implements NormalizerInterface
+{
+    /**
+     * @param FormException $exception
+     * @param null          $format
+     *
+     * @return array|bool|float|int|string|void
+     */
+    public function normalize($exception, $format = null, array $context = [])
+    {
+        $data = [];
+        $errors = $exception->getErrors();
+
+        foreach ($errors as $error) {
+            $data[$error->getOrigin()->getName()][] = $error->getMessage();
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param mixed $data
+     * @param null  $format
+     *
+     * @return bool|void
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof FormException;
+    }
+}

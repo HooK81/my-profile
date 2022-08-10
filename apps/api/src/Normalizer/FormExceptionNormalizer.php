0d2baddef5cc54c6eec8d7a8bfcee42c4cd7ef7e
Julien CROCHET <julien@crochet.me>
2022-08-10 21:44:51 +0200

    symfony 6.1 upgrade

--- apps/api/src/Normalizer/FormExceptionNormalizer.php
+++ apps/api/src/Normalizer/FormExceptionNormalizer.php
@@ -31,7 +31,7 @@ class FormExceptionNormalizer implements NormalizerInterface
         return [$data];
     }
 
-    public function supportsNormalization($data, string $format = null): bool
+    public function supportsNormalization($data, string $format = null, array $context = []): bool
     {
         return $data instanceof FormException;
     }
